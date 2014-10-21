ae5ba4a6adc2423f30f11f62ab717d9cf55f412b
Pieter Ennes <pieter@ennes.nl>
2014-10-21 22:07:47 +0100

    Fix test for new exception.

--- tests/test_jwt.py
+++ tests/test_jwt.py
@@ -393,7 +393,7 @@ class TestJWT(unittest.TestCase):
         jwt_message = jwt.encode(self.payload, key=None, algorithm=None)
 
         self.assertRaises(
-            NotImplementedError,
+            jwt.DecodeError,
             lambda: jwt.decode(jwt_message))
 
         jwt.decode(jwt_message, verify=False)
